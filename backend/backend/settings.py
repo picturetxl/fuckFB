e545cd0766d25e2e12adaed982230f45a06a11b2
picturetxl <txltech123@163.com>
2022-05-02 00:14:00 +0800

    添加mysql配置和虚拟环境

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "base.apps.BaseConfig", # install app
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,9 +75,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default':{
+        'ENGINE':'django.db.backends.mysql',
+        'NAME':'fb',
+        'USER':'root',
+        'PASSWORD':'helloworld',
+        'HOST':'127.0.0.0.1',
+        'PORT':'3306'
     }
 }
 
